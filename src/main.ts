f4f47ba67226a900599cba7dd31bb7b45f31de39
osseasofertas <fabianooseas23@gmail.com>
2025-07-25 16:12:02 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ async function bootstrap() {
       'http://localhost:5173',
       'https://platform-production-f017.up.railway.app',
       'https://ospltform-front.vercel.app',
-      'https://onlyplatformreviewer.vercel.app/' 
-      
+      'https://onlyplatformreviewer.vercel.app'
     ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
