2f5055eb3ce36171d176d423ca895c9af46108fe
Edmilson Romano <edmilsonromano14@gmail.com>
2025-07-24 23:43:41 -0300

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-app.enableCors({
-  origin: [
-    'http://localhost:5173',
-    'https://platform-production-f017.up.railway.app',
-  ],
-  credentials: true,
-});
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://platform-production-f017.up.railway.app',
+    ],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
